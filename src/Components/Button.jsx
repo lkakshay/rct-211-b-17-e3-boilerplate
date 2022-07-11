04c76787dac4fe48dfe2d10e458c567b96b8e8a8
lkakshay <connectme.lkakshay@gmail.com>
2022-07-11 16:49:30 +0530

    ass

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,3 +1,85 @@
-export const Button = ({ colorScheme, variant, size }) => {
-  return <button></button>;
+import { useEffect, useState } from "react";
+
+const color=['red', 'yellow', 'orange', 'green']
+const sizes=["sm", "md", "lg", "xl"]
+const variant=["solid", "ghost", "outline"]
+
+export const Button = ({ colorScheme , variant, size }) => {
+
+  const [style,setStyle]=useState({
+    backgroundColor:"white",
+    padding:"20px",
+    border:"1px solid black",
+
+    
+  })
+ 
+
+  const check=(x,arr)=>{
+    let a =false
+    
+    arr.forEach((e)=>{
+      if(e===x){
+        
+        a= true
+      }
+    })
+    
+
+    if(a)
+    return true
+
+    else
+    return false
+  }
+  
+
+  const setSize=(x)=>{
+    if(x==="sm")
+    setStyle({...style,padding:"10px"})
+
+    if(x==="lg")
+    setStyle({...style,padding:"30px"})
+
+    if(x==="xl")
+    setStyle({...style,padding:"40px"})
+
+  }
+ 
+
+
+ 
+
+  
+useEffect(()=>{
+  if(colorScheme){
+
+  if(check(colorScheme,color))
+  setStyle({...style,backgroundColor:colorScheme})
+  
+  else
+  setStyle({...style,backgroundColor:"white"})
+  } 
+
+},[colorScheme])
+
+
+useEffect(()=>{
+  if(size){
+    if(check(size,sizes))
+    setSize(size)
+    
+  else
+   setStyle({...style,padding:"20px"})
+  }
+},[size])
+console.log(style)
+
+
+  return <button style={style} >
+    
+  </button>;
 };
+
+
+
--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,1 +1,36 @@
-export const useCounter = (init) => {};
+import { useState } from "react";
+
+export const useCounter = ({
+    initialValue,minValue,maxValue
+  }) => {
+   
+    
+    const[count,setCount]=useState(initialValue)
+
+    const incCount=(x)=>{
+       
+        if(x){
+            if(count+x<=maxValue)
+            setCount(count+x)
+        }
+        else{
+            if(count+1<=maxValue)
+            setCount(count+1)
+        }
+    }
+
+    const decCount=(x)=>{
+       
+        if(x){
+            if(count-x>=minValue)
+            setCount(count-x)
+        }
+        else{
+            if(count-x>=minValue)
+            setCount(count-1)
+        }
+    }
+
+    return{count,incCount,decCount}
+
+  };
